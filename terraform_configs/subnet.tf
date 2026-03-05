945b5cbd373868d795ac2f41988ba293643cb254
Ismail Khan <ismail.khan02@gmail.com>
2026-03-05 15:30:06 +0000

    Added Web Server Subnets 1&2

--- terraform_configs/subnet.tf
+++ terraform_configs/subnet.tf
@@ -1,4 +1,4 @@
-# Creating 1st Web Subnet (Public)
+# Creating ALB Public Subnet 1
 resource "aws_subnet" "public-subnet-1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_cidrs[0]
@@ -20,6 +20,33 @@ resource "aws_subnet" "public-subnet-2" {
 
   tags = {
     Name        = "alb-public-subnet-2"
+    Environment = var.environment
+  }
+}
+
+# Creating Web Subnet 1 (Private)
+resource "aws_subnet" "web-subnet-1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.web_subnet_cidrs[0]
+  availability_zone = var.availability_zones[0]
+
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "web-subnet-1"
+    Environment = var.environment
+  }
+}
+
+# Creating Web Subnet 2 (Private)
+resource "aws_subnet" "web-subnet-2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.web_subnet_cidrs[1]
+  availability_zone       = var.availability_zones[1]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "web-subnet-2"
     Environment = var.environment
   }
 }
@@ -53,7 +80,7 @@ resource "aws_subnet" "application-subnet-2" {
 
 resource "aws_db_subnet_group" "mydb-subnet-group" {
   name       = "mydb-subnet-group"
-  subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
+  subnet_ids = [aws_subnet.application-subnet-1.id, aws_subnet.application-subnet-2.id]
 
   tags = {
     Name        = "mydb-subnet-group"

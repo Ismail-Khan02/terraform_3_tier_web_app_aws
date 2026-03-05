3a244a99440f99a86ab5786485e8b06d2aa27775
Ismail Khan <ismail.khan02@gmail.com>
2026-03-05 14:58:15 +0000

    Configured Autoscaling Group for App Servers

--- terraform_configs/app_ec2.tf
+++ terraform_configs/app_ec2.tf
@@ -1,16 +1,17 @@
-resource "aws_instance" "app_server" {
-  count         = 2
-  ami           = var.ec2_ami
+# App Tier Launch Template
+resource "aws_launch_template" "app_launch_template" {
+  name_prefix   = "app-launch-template-"
+  image_id      = var.ec2_ami
   instance_type = "t2.micro"
 
-  # Logic: Distribute across App Subnet 1 and App Subnet 2
-  subnet_id = count.index == 0 ? aws_subnet.application-subnet-1.id : aws_subnet.application-subnet-2.id
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.app_sg.id]
+  }
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ssm_profile.name
+  }
 
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
-
-  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
-
-  # SCRIPT: Run the Node.js install script
   user_data = templatefile("app_data.sh", {
     db_host = aws_db_instance.mydb.address
     db_user = var.db_username
@@ -18,8 +19,46 @@ resource "aws_instance" "app_server" {
     db_name = var.db_name
   })
 
-  tags = {
-    Name        = "app-server-${count.index + 1}"
-    Environment = var.environment
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name        = "app-server"
+      Environment = var.environment
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [aws_db_instance.mydb]
+  
+}
+
+# App Tier Auto Scaling Group
+resource "aws_autoscaling_group" "app_asg" {
+  name                      = "app-asg"
+  max_size                  = 4
+  min_size                  = 2
+  desired_capacity          = 2
+  launch_template {
+    id      = aws_launch_template.app_launch_template.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier       = [aws_subnet.application-subnet-1.id, aws_subnet.application-subnet-2.id]
+  target_group_arns         = [aws_lb_target_group.internal_alb_tg.arn]
+
+  tag {
+    key                 = "Name"
+    value               = "app-server-asg"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = var.environment
+    propagate_at_launch = true
+
   }
 }
+
